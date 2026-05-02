3e91189dde2bfee02888f6260dfc50c9b96837bf
Muhammad Aslam <mashoriaslam011@gmail.com>
2026-05-02 10:38:06 +0500

    Sum of three values

---
+++ math.java
@@ -0,0 +1,9 @@
+class math {
+    public static void main(String[] args) {
+      int num1 = 32;
+      int num2 = 16;
+      int num3 = 40;
+      int sum = num1 + num2 + num3;
+      System.out.println(sum);
+    }
+}
